ede5141d78d147131c6035fe99fa89414d7ced40
hoge git <git.test@gmail.com>
2024-05-20 23:05:05 +0900

    balde end

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -15,13 +15,13 @@
             <tr class="confirm-table__row">
               <th class="confirm-table__header">お名前</th>
               <td class="confirm-table__text">
-                <input type="text" name="name" value="{{$contact['name]}}" readonly >
+                <input type="text" name="name" value="{{$contact['name']}}" readonly >
               </td>
             </tr>
             <tr class="confirm-table__row">
               <th class="confirm-table__header">メールアドレス</th>
               <td class="confirm-table__text">
-                <input type="email" name="email" value="{{ $contact['email'] }}" readonly />
+                 <input type="email" name="email" value="{{ $contact['email'] }}" readonly />
               </td>
             </tr>
             <tr class="confirm-table__row">
@@ -33,7 +33,7 @@
             <tr class="confirm-table__row">
               <th class="confirm-table__header">お問い合わせ内容</th>
               <td class="confirm-table__text">
-                <input type="text" name="content" value="{{ $contact['content'] }}" readonly  />
+                <input type="text" name="content" value="{{ $contact['content'] }}" readonly />
               </td>
             </tr>
           </table>
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('css')
-  <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/index.css') }}" >
 @endsection
 
 @section('content')
@@ -10,7 +10,7 @@
       <div class="contact-form__heading">
         <h2>お問い合わせ</h2>
       </div>
-      <form class="form" action="/contacts/confirm" method="post">
+      <form class="form" action="contacts/confirm" method="post">
         @csrf
         <div class="form__group">
           <div class="form__group-title">
@@ -19,7 +19,7 @@
           </div>
           <div class="form__group-content">
             <div class="form__input--text">
-              <input type="text" name="name" placeholder="テスト太郎" value="{{ old('name') }}" "/>
+              <input type="text" name="name" placeholder="テスト太郎" value="{{ old('name') }}" />
             </div>
             <div class="form__error">
              @error('name')
@@ -35,10 +35,10 @@
           </div>
           <div class="form__group-content">
             <div class="form__input--text">
-              <input type="email" name="email" placeholder="test@example.com" value="{{ old('email') }}" "/>
+              <input type="email" name="email" placeholder="test@example.com" value="{{ old('email') }}" />
             </div>
             <div class="form__error">
-             @error('name')
+             @error('email')
              {{$message}}
              @enderror
             </div>
@@ -54,7 +54,7 @@
               <input type="tel" name="tel" placeholder="09012345678" value="{{ old('tel') }}" />
             </div>
             <div class="form__error">
-             @error('name')
+             @error('tel')
              {{$message}}
              @enderror
             </div>
@@ -66,7 +66,7 @@
           </div>
           <div class="form__group-content">
             <div class="form__input--textarea">
-              <textarea name="content" placeholder="資料をいただきたいです"></textarea>
+              <textarea name="content" placeholder="資料をいただきたいです">{{ old('content') }}</textarea>
             </div>
           </div>
         </div>
